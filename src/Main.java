42a0fdfad8e20551c8d12ab94f1018bceb7fbf75
theseventhshadow <the_angeloz_@hotmail.com>
2025-05-13 15:58:28 -0400

    Proyecto1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> opciones = new ArrayList<>();
+        Random random = new Random();
+        boolean b = true;
+
+        System.out.println("Cuantas opciones tienes?: ");
+        int s;
+        try {
+             s = Integer.parseInt(sc.nextLine());
+        }catch (NumberFormatException e){
+            System.out.println("Ingresa valores numericos");
+            return;
         }
+            for (int i = 0; i < s; i++) {
+                System.out.println("Ingresa las opcion " + (i + 1) + " que tienes: ");
+                String op = sc.nextLine();
+                opciones.add(op);
+
+                if (s == 0) {
+                    b = false;
+                }
+            }
+
+
+        int indiceAleatorio = random.nextInt(opciones.size());
+        System.out.println("Se ha seleccionado la opcion: " + opciones.get(indiceAleatorio));
+
+        }
+
     }
-}
